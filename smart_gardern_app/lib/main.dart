bae4b0467bbbdcb4c6f2bb72d0531a729c7b2794
Phạm Minh Hiếu <54505544+AagonP@users.noreply.github.com>
2021-05-15 22:12:39 +0700

    Update Branch

---
+++ smart_gardern_app/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:smart_gardern_app/constant.dart';
+import 'package:smart_gardern_app/screens/Welcome/welcome_screen.dart';
+import 'dart:math';
+import 'mqtt.dart';
+import 'dart:async';
+
+//firebase libs
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+// Testing MQTT package
+void initTest() async {
+  await MqttHelper.connect();
+  await MqttHelper.subcribe('test/json');
+  // var rng = new Random();
+  // for (int i = 0; i < 10; i++) {
+  //   MqttHelper.publish('moisture-sensor', rng.nextInt(100).toString());
+  // }
+  Map data = {"id": "1", "name": "LED", "data": "58", "unit": ""};
+  MqttHelper.publish('test/json', data.toString());
+}
+
+void main() {
+  // Remove this when done testing
+  initTest();
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          // return SomethingWentWrong();
+          return Container();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container();
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //Don't display banner when debugging
+      title: 'Smart Farm',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
