fd8fcfd4d42616e3a3521612a949bfec4c4c1d82
Phạm Minh Hiếu <54505544+AagonP@users.noreply.github.com>
2021-06-21 22:32:57 +0700

    Revert "Revert "Adjust some PlantLib UI""
    This reverts commit 2028cdfb736b8f0cb9acc51401f84b44ff147a56.

--- smart_gardern_app/lib/Screens/Plant Library/Overview/components/body.dart
+++ smart_gardern_app/lib/Screens/Plant Library/Overview/components/body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_gardern_app/Screens/Plant%20Library/PlantIdentification/plant_identification_screen.dart';
 import 'package:smart_gardern_app/Screens/Plant%20Library/SavedPlants/saved_plants.dart';
 import 'package:smart_gardern_app/components/custtom_icon_button.dart';
 
+import '../../../../constant.dart';
 import 'explore_bar.dart';
 import 'plant_category_bar.dart';
 import '../components/top_bar.dart';
@@ -36,19 +38,26 @@ class Body extends StatelessWidget {
                 ),
                 Padding(
                     padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5)),
-                CustomIconButton(
-                  icon: Icons.search,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return PlantIdentificationScreen();
-                        },
-                      ),
-                    );
-                  },
-                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: kPrimaryColor,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PlantIdentificationScreen();
+                          },
+                        ),
+                      );
+                    },
+                    icon: SvgPicture.asset('assets/icons/plant_search.svg',
+                        color: Colors.white),
+                  ),
+                )
               ],
             ),
             HeaderText(
